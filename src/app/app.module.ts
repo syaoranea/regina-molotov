e51db28385f630514c425566c8a21c26eeb3ddc4
Eduardo Rocha <eduardo_rocha87@hotmail.com>
2023-02-28 19:58:40 -0300

    feat(app): desenvolvimento heading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LandingPageModule } from './landing-page/landing-page.module';
+import { PageErrorModule } from './page-error/page-error.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LandingPageModule,
+    PageErrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './views/landing-page.component';
+import { HeadingComponent } from './components/heading/heading.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    HeadingComponent
   ],
   imports: [
     CommonModule
